d07bb853c1bdb1f9f91743806e285f5bb9402d7c
Theprocyon <theprocyon@outlook.kr>
2023-11-25 19:34:20 +0900

    - project 생성

---
+++ bit_counting/bit_counting.srcs/sources_1/new/bit_counter.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/11/25 19:13:50
+// Design Name: 
+// Module Name: bit_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bit_count();
+endmodule
